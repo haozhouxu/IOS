7dab44e53f455940f65504a09bf7970a2d42c573
haozhouxu <470001421@qq.com>
2016-01-14 10:08:00 +0800

    增加一个model的文件夹

--- Table/ViewController.cs
+++ Table/ViewController.cs
@@ -17,7 +17,6 @@ namespace Table
             //table1 = new UITableView(View.Bounds); // defaults to Plain style
             string[] tableItems = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
             table1.Source = new TableSource(tableItems);
-            table1.selectr
         }
 
         public override void DidReceiveMemoryWarning()
